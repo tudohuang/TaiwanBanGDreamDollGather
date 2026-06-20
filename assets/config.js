60d1940065c3ef8afd127e75a102ca39d7ed0f2d
TudoHuang <88125758+tudohuang@users.noreply.github.com>
2026-06-20 18:41:41 +0800

    Create config.js

---
+++ assets/config.js
@@ -0,0 +1,9 @@
+window.SITE_CONFIG = {
+  SHEET_ID: "1GJbpyT4qjxUvANqGeG0qfECuWEgbM2aWp9Xo_yKuhm8",
+  SHEET_NAME: "",
+
+  TITLE: "娃聚地圖",
+  SUBTITLE: "把散落各處的娃聚，編成一份索引。挑場近的、順路的，帶上你的娃一起去。",
+
+  FORM_URL: "https://docs.google.com/forms/d/e/1FAIpQLSfwayQC0f_dlXbhHepqeQpwlBL7C0ikJhVIwXFJgKKkQA078Q/viewform",
+};
